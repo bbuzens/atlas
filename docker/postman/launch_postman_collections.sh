1525b5ba028b49e857adb9094e6afb4af6a9d458
bbuzens <benoit.buzens@gmail.com>
2021-02-18 18:52:34 +0100

    Ajout postman multilaunch

--- docker/postman/launch_postman_collections.sh
+++ docker/postman/launch_postman_collections.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-
 # Forcer la sortie en cas d'erreur
 set -e
+#Debug mode
+#set -x
 
 # Version de Newman
 echo "## VERSION NEWMAN ##"
 newman --version
 
 # Find Postman Collections
-collections=$(find . -name *.postman_collection.json)
+collections=$(find . | grep postman_collection.json)
 #echo "## COLLECTIONS FILES"
 echo $collections
 
@@ -17,6 +18,6 @@ echo "## NEWMAN RUN ##"
 echo "START Running NEWMAN on `date`"
 for collection in $collections
   do
-    newman $collection --reporters cli,junit --reporter-junit-export $collection_junit_report.xml
+    newman run $collection --reporters cli,junit --reporter-junit-export $(basename -- $collection)_junit_report.xml
 done
 echo "END Running NEWMAN on `date`"
