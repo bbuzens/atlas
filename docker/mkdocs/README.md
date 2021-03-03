e0b086d78aabf921092569289e112a4baa0bd3e2
bbuzens <benoit.buzens@gmail.com>
2021-03-03 15:51:50 +0100

    ajouts : postman ; mkdocs ; soapui

---
+++ docker/mkdocs/README.md
@@ -0,0 +1,17 @@
+#Docker MkDocs
+
+Un conteneur MkDocs parce que publier de la doc c'est bien.
+
+# Utilisation
+
+* Tu pulles le projet
+* Tu pulles ta doc dans un répertoire "docs" sous la racine
+* Tu lances la commande ``mkdocs serve``
+
+Voilà... c'est à peu près tout. 
+
+Si tu veux changer la conf, ça se passe dans *mkdocs.yml*.
+
+>Pour plus d'info, ça se passe sur le site [MkDocs](https://www.mkdocs.org/)
+
+PS : Comme d'hab, un grand merci aux devs qui mettent ce type de produit librement à dispo de la communauté.
--- docker/postman/launch_postman_collections.sh
+++ docker/postman/launch_postman_collections.sh
@@ -13,11 +13,11 @@ collections=$(find . | grep postman_collection.json)
 #echo "## COLLECTIONS FILES"
 echo $collections
 
-# Keep entrypoint simple: we must pass the standard JMeter arguments
+# Run all collections founded and produce a report for each
 echo "## NEWMAN RUN ##"
 echo "START Running NEWMAN on `date`"
 for collection in $collections
   do
-    newman run $collection --reporters cli,junit --reporter-junit-export $(basename -- $collection)_junit_report.xml
+    newman run $collection --reporters cli,junit --reporter-junit-export $(basename -- "$collection" .postman_collection.json)_junit_report.xml
 done
 echo "END Running NEWMAN on `date`"
